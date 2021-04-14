06db917348757ec380430cc8b7f89d3aa78fed4a
Solacx <junyong6@gmail.com>
2021-04-14 10:14:37 +1000

    Camera now tracks player movement.

---
+++ Assets/Scripts/Camera/FollowObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] private GameObject temp;   // Use a better name
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            temp.transform.position.x,
+            temp.transform.position.y,
+            gameObject.transform.position.z
+        );
+    }
+}
