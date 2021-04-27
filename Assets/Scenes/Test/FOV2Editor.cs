4c21c886fb2e95752b2d73532aeba1242f477824
Solacx <junyong6@gmail.com>
2021-04-28 02:51:05 +1000

    WIP: Render FOV as visible area.
    Function works but code needs to be cleaned up.

---
+++ Assets/Scenes/Test/FOV2Editor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FOV2))]
+public class FOV2Editor : Editor
+{
+    void OnSceneGUI() {
+        FOV2 view = (FOV2) target;
+
+        Handles.color = Color.white;
+        Handles.DrawWireArc(view.transform.position, Vector3.forward, Vector3.right, 360, view.viewRadius);
+
+        Vector3 viewAngleA = view.DirFromAngle(-view.viewAngle / 2, false);
+        Vector3 viewAngleB = view.DirFromAngle(view.viewAngle / 2, false);
+
+        Handles.DrawLine(view.transform.position, view.transform.position + viewAngleA * view.viewRadius);
+        Handles.DrawLine(view.transform.position, view.transform.position + viewAngleB * view.viewRadius);
+    }
+}
