6a094ef598c4372b486098e41410ac3b9eefbd12
Solacx <junyong6@gmail.com>
2021-05-16 18:07:50 +1000

    Add pause menu.
    Pause menu is achieved using a persistent menu controller, canvas and
    event system. Each object should be destroyed when loading the main
    menu scene again.
    
    NB: Known issue where event system object is created multiple times.

---
+++ Assets/Scripts/Persistence/Persistent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistent : MonoBehaviour
+{
+    void Start() {
+        DontDestroyOnLoad(this);
+    }
+}
